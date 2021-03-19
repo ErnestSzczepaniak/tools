d36b51110a2195fbf51eb71b40c7504c9a211dce
en2 <ernest.szczepaniak.en2@gmail.com>
2021-03-19 13:53:05 +0100

    push

--- cmake/CMakeConfigCompilation.cmake
+++ cmake/CMakeConfigCompilation.cmake
@@ -17,7 +17,7 @@ set(private_definition_host     )
 
 # ---------------------------------------------| option |--------------------------------------------- #
 
-set(public_option_common        -std=c++2a -Wno-int-to-pointer-cast -fno-operator-names -Wno-write-strings)
+set(public_option_common        -Wno-int-to-pointer-cast -fno-operator-names -Wno-write-strings)
 set(public_option_target        )
 set(public_option_host          )
 
